a948f39fd52e00584427cd203290ba4544d13b4f
brandonjpb <brandon@sterba.net>
2025-05-14 14:48:55 -0700

    Delete out/build/x64-Debug directory
    Deleted accidentally included build files

--- out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/brand/OneDrive/Desktop/Programs/CIS-25/Red Shift Weaponizer
-# Build directory: C:/Users/brand/OneDrive/Desktop/Programs/CIS-25/Red Shift Weaponizer/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
--- out/build/x64-Debug/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/brand/OneDrive/Desktop/Programs/CIS-25/Red Shift Weaponizer/test
-# Build directory: C:/Users/brand/OneDrive/Desktop/Programs/CIS-25/Red Shift Weaponizer/out/build/x64-Debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(WeaponTests "C:/Users/brand/OneDrive/Desktop/Programs/CIS-25/Red Shift Weaponizer/out/build/x64-Debug/test/RedShiftTests.exe")
-set_tests_properties(WeaponTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/brand/OneDrive/Desktop/Programs/CIS-25/Red Shift Weaponizer/test/CMakeLists.txt;12;add_test;C:/Users/brand/OneDrive/Desktop/Programs/CIS-25/Red Shift Weaponizer/test/CMakeLists.txt;0;")

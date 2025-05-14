d4b1df11598f5b0abb69d066a9a23d8a81f3adee
brandonjpb <brandon@sterba.net>
2025-05-14 14:42:56 -0700

    Slight file modifications for clarity

--- out/build/x64-Debug/cmake_install.cmake
+++ out/build/x64-Debug/cmake_install.cmake
@@ -37,6 +37,12 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("C:/Users/brand/OneDrive/Desktop/Programs/CIS-25/Red Shift Weaponizer/out/build/x64-Debug/test/cmake_install.cmake")
 endif()
 
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+if(CMAKE_INSTALL_LOCAL_ONLY)
+  file(WRITE "C:/Users/brand/OneDrive/Desktop/Programs/CIS-25/Red Shift Weaponizer/out/build/x64-Debug/install_local_manifest.txt"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
+endif()
 if(CMAKE_INSTALL_COMPONENT)
   if(CMAKE_INSTALL_COMPONENT MATCHES "^[a-zA-Z0-9_.+-]+$")
     set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
@@ -50,8 +56,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
   file(WRITE "C:/Users/brand/OneDrive/Desktop/Programs/CIS-25/Red Shift Weaponizer/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
